04672326b57db0ddd94257c80b8cbad43876e9f2
Nurul-GC <nuruldecarvalho@gmail.com>
2022-05-11 22:31:08 +0100

    update lgc/*

---
+++ lgc/lgcmain.py
@@ -0,0 +1,91 @@
+from sys import argv, exit
+import webbrowser
+
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+
+temaescuro = open('lgc-theme/dark.qss').read().strip()
+temaclaro = open('lgc-theme/light.qss').read().strip()
+
+
+class LGC:
+    def __init__(self):
+        self.gc = QApplication(argv)
+        QFontDatabase.addApplicationFont('lgc-fonts/Kranky.ttf')
+
+        self.ferramentas = QWidget()
+        self.ferramentas.setWindowTitle('lanternGC')
+        self.ferramentas.setWindowState(Qt.WindowState.WindowMaximized)
+        self.ferramentas.setWindowFlags(Qt.WindowType.WindowMinimizeButtonHint)
+        self.ferramentas.setStyleSheet(temaescuro)
+
+        self.layout = QVBoxLayout()
+
+        menu = QMenuBar()
+        detalhes = menu.addMenu('Details')
+        instr = detalhes.addAction('Instructions')
+        instr.triggered.connect(self._instr)
+        detalhes.addSeparator()
+        sair = detalhes.addAction('Exit')
+        sair.triggered.connect(self._sair)
+        sobre = menu.addAction('About')
+        sobre.triggered.connect(self._sobre)
+        self.layout.setMenuBar(menu)
+        self.ferramentas.setLayout(self.layout)
+
+        self.principal()
+
+        link = lambda: webbrowser.open_new('https://artesgc.home.blog')
+        copyrightlabel = QLabel('<a style="text-decoration: none;" href="#">&trade; ArtesGC, Inc</a>')
+        copyrightlabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        copyrightlabel.linkActivated.connect(link)
+        copyrightlabel.setToolTip("Open ArtesGC's oficial website!")
+        barramenu = QToolBar("Copyright")
+        barramenu.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
+        barramenu.addWidget(copyrightlabel)
+        self.layout.addWidget(barramenu)
+
+    def principal(self):
+        def ligar():
+            if lantern_btn.isChecked():
+                lantern_btn.setText('&OFF')
+                self.ferramentas.setStyleSheet(temaclaro)
+            else:
+                lantern_btn.setText('&ON')
+                self.ferramentas.setStyleSheet(temaescuro)
+
+        frame = QFrame()
+        layout = QVBoxLayout()
+
+        lantern_btn = QPushButton('&ON')
+        lantern_btn.setFixedHeight(550)
+        lantern_btn.setAutoExclusive(True)
+        lantern_btn.setCheckable(True)
+        lantern_btn.clicked.connect(ligar)
+        layout.addWidget(lantern_btn)
+
+        frame.setLayout(layout)
+        self.layout.addWidget(frame)
+
+    def _sobre(self):
+        QMessageBox.information(self.ferramentas, 'About', '''<h2>Information about the Program</h2><hr>
+        Name: <b>GC-lantern (LGC)</b><br>
+        Version: <b>0.1-052022</b><br>
+        Programmer & Designer: <b>Nurul-GC</b><br>
+        Company: <b>ArtesGC, Inc.</b>''')
+
+    def _instr(self):
+        QMessageBox.information(self.ferramentas, 'Instructions', '''<h2>Quick Presentation</h2><hr>
+        <p>Hello Dear User!<br>
+        I present to you a very practical and useful tool,
+        a program that works as a lantern on your desktop/tablet PC..</p>
+        <p>It's easy to be used, you just need to push the button and it will change his lgc-theme,
+        helping you to see what's around you!</p>
+        <p>Thanks for your support!<br>
+        <b>&trade;ArtesGC, Inc.</b></p>''')
+
+    def _sair(self):
+        pergunta = QMessageBox.question(self.ferramentas, 'Confirm Exit', '<b>Do you really want to close the program?</b>')
+        if pergunta == pergunta.Yes:
+            exit(0)
