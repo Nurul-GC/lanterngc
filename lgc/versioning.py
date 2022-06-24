1ea9ca68188507fe2b7343fce25966c409349cb3
Nurul-GC <nuruldecarvalho@gmail.com>
2022-06-24 14:54:07 +0100

    update lgc/*

---
+++ lgc/versioning.py
@@ -0,0 +1,12 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="versionfile.txt",
+    version="0.2.062022",
+    company_name="ArtesGC Inc",
+    file_description="simple tool for tablet/desktop PCs",
+    internal_name="lantern tool for PCs",
+    legal_copyright="© Nurul-GC. All rights reserved.",
+    original_filename="lgc",
+    product_name="LanternGC"
+)
